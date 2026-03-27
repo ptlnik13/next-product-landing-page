92352ea75ba8d845d7553bfd8fdcf17c5da1fc7d
ptlnik13 <nikeshvpatel@gmail.com>
2026-03-27 12:49:08 -0400

    Formatted necessary data extraction

--- utils/catalog.js
+++ utils/catalog.js
@@ -8,8 +8,28 @@ async function getEntries(content_type, order) {
 export async function getCatalogItems() {
     try {
         const response = await getEntries("catalogItem", "fields.title");
-        // console.log("getCatalogItems response:", response);
-        return response.items;
+
+        return response?.items?.map(item => {
+            const {fields = {}} = item;
+            // console.log(fields);
+
+            return {
+                id            : item.sys?.id,
+                title         : fields?.title || "",
+                slug          : fields?.slug || "",
+                imageUrl      : fields?.image?.fields?.file?.url ? `https:${fields.image.fields.file.url}` : "",
+                priceLoggedOut: fields?.priceLoggedOut ?? 0,
+                priceLoggedIn : fields?.priceLoggedIn ?? 0,
+                category      : fields?.category || "",
+                cardSet       : fields?.cardSet || "A",
+                attributes    : {
+                    rating  : fields?.rating ?? 0,
+                    color   : fields?.color || "",
+                    size    : fields?.size || "",
+                    material: fields?.material || "",
+                },
+            }
+        })
     } catch (e) {
         console.error("getCatalogItems fetch error:", e);
         return [];
@@ -19,8 +39,19 @@ export async function getCatalogItems() {
 export async function getDisplayMappings() {
     try {
         const response = await getEntries("displayMapping", "fields.name");
-        // console.log("getDisplayMappings response:", response);
-        return response.items;
+
+        return response?.items?.map((item) => {
+            const {fields = {}} = item;
+
+            return {
+                id                 : item.sys?.id,
+                name               : fields?.name || "",
+                key                : fields?.key || "",
+                showPriceFirst     : fields?.showPriceFirst ?? true,
+                showBadges         : fields?.showBadges ?? true,
+                highlightAttributes: fields?.highlightAttributes || [],
+            };
+        });
     } catch (e) {
         console.error("getDisplayMappings fetch error:", e);
         return [];
